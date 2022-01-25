517d34284fbad05209d8dfaf3bc31866f73c9f90
rezwan <rezwan6@gmail.com>
2022-01-25 08:08:16 +0600

    I'm stuck here!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './default.scss';
 import Header from './components/Header';
+import Homepage from './pages/Homepage';
+
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <h2>Hello World</h2>
+      <div className='main'>
+        <Homepage></Homepage>
+      </div>
     </div>
   );
 }
---
+++ src/components/Directory/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+// purpose of this component to have two images with two links
+import './styles.scss';
+import phone from '../../images/phone.png';
+import computer from '../../images/computer.jpg';
+
+const Directory = props => {
+    return (
+        <div className='directory'>
+            <div className='wrap'>
+                <div
+                    className='item'
+                    style={{
+                        backgroundImage: `url(${phone})`
+                    }}
+                >
+                    <a href="">Phone Gadgets</a>
+                </div>
+                <div
+                    className='item'
+                    style={{
+                        backgroundImage: `url(${computer})`
+                    }}
+                >
+                    <a href="">Computer Gadgets</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Directory;
